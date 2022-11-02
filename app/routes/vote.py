a007bf5d01faa1d0d94d09fef4185d747ce8610e
wellingtonpecchini <wpecchini@alteanet.it>
2022-11-02 22:51:17 +0100

    change post_id vote

--- app/routes/vote.py
+++ app/routes/vote.py
@@ -11,13 +11,13 @@ router = APIRouter(prefix='/votes', tags=['votes'])
 @router.post('/', status_code=status.HTTP_201_CREATED)
 def add_vote(vote: schemas.Vote, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
 
-    post = db.query(models.Post).filter(models.Post.id == vote.post_id)
+    post = db.query(models.Post).filter(models.Post.id == vote.posts_id)
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"post doesn't exist")
 
-    votes = db.query(models.Vote).filter(models.Vote.post_id ==
-                                         vote.post_id, models.Vote.user_id == current_user.id)
+    votes = db.query(models.Vote).filter(models.Vote.posts_id ==
+                                         vote.posts_id, models.Vote.user_id == current_user.id)
 
     found_votes = votes.first()
 
@@ -26,7 +26,7 @@ def add_vote(vote: schemas.Vote, db: Session = Depends(get_db), current_user: in
             raise HTTPException(status_code=status.HTTP_409_CONFLICT,
                                 detail=f"user {current_user.email} as already voted {found_votes.id}")
 
-        new_vote = models.Vote(post_id=vote.post_id,
+        new_vote = models.Vote(posts_id=vote.posts_id,
                                user_id=current_user.id)
         db.add(new_vote)
         db.commit()
